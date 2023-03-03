2e906c6d3d6c13df85a23842e8c614a673435905
yoavweber <yoav.weber@code.berlin>
2023-03-03 13:30:57 +0200

    store entities location in state machine

--- src/Game/stateMachine.tsx
+++ src/Game/stateMachine.tsx
@@ -1,6 +1,8 @@
 import { assign, createMachine } from "xstate";
 import { initBoard, GenBoardLocationPerEntitiy } from "../Board/location";
 import { Board } from "../Board/board";
+import { Maybe } from "../logic/types";
+import { Location } from "../Enteties/enteties";
 
 export function createGameMachine(
   genBoardLocation: GenBoardLocationPerEntitiy
@@ -8,28 +10,51 @@ export function createGameMachine(
   return createMachine({
     initial: "idle",
     schema: {
-      context: {} as { board: Board | null },
+      context: {} as {
+        board: Maybe<Board>;
+        userLocation: Maybe<Location>;
+        PbLocation: Maybe<Location>;
+        GPgpLocation: Maybe<Location[]>;
+      },
     },
     context: {
       board: null,
+      userLocation: null,
+      PbLocation: null,
+      GPgpLocation: null,
     },
     states: {
       idle: {
         on: {
           START: {
-            target: "startGame",
-            actions: assign({
-              board: (context, event) =>
-                (context.board = initBoard(genBoardLocation)), //TODO: accept genrandomboard as parameter
+            target: "playGame",
+            actions: assign((context) => {
+              const [board, [userLocation, PbLocation, GPgpLocation]] =
+                initBoard(genBoardLocation);
+              context.board = board;
+              context.userLocation = userLocation[0];
+              context.PbLocation = PbLocation[0];
+              context.GPgpLocation = GPgpLocation;
+
+              return {
+                board: board,
+                userLocation: userLocation[0],
+                PbLocation: PbLocation[0],
+                GPgpLocation: GPgpLocation,
+              };
             }),
           },
         },
       },
-      startGame: {
+      playGame: {
         type: "final",
       },
-      // playGame: {},
-      // endGame: {},
+      // on: MAKE_MOVE: {
+
+      // }
+      // },
+      // win: { type: 'final' },
+      // lose: { type: 'final' }
     },
   });
 }
