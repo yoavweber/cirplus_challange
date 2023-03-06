4de15f102db7c75b4a6dae80aafedd4e5bb71abc
yoavweber <yoav.weber@code.berlin>
2023-03-06 15:02:54 +0200

    adjusting cell color accourding to the entity

--- src/view/gameBoard/Cell.tsx
+++ src/view/gameBoard/Cell.tsx
@@ -5,12 +5,12 @@ interface CellProps {
   entity: Entities;
 }
 
-const Colors = ["white", "blue", "green", "red"];
+const Colors = ["white", "blue", "green", "rgb(242,167,167)"];
 export const Cell: React.FC<CellProps> = ({ entity }) => {
   return (
     <div
       style={{
-        border: "1px solid",
+        border: ".5px solid",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
@@ -18,8 +18,8 @@ export const Cell: React.FC<CellProps> = ({ entity }) => {
         width: "30px",
         backgroundColor: `${Colors[entity]}`,
       }}
-    >
-      {entity != 0 && entity}
-    </div>
+    />
+
+    // </div>
   );
 };
