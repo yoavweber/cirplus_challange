924a67fb00a071f7b9586f3b4140183a06956cce
yoavweber <yoav.weber@code.berlin>
2023-03-06 15:26:28 +0200

    handling winning state

--- src/view/LastBottlePage.tsx
+++ src/view/LastBottlePage.tsx
@@ -1,19 +1,29 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useEffect } from "react";
 
 import { useMachine } from "@xstate/react";
 import { createIOMachine } from "../logic/io";
 import { GameBoard } from "./gameBoard/GameBoard";
 import { Header } from "./header/Header";
+import { Footer } from "./footer/Footer";
 
 export const LastBottlePage: React.FC = () => {
   const machine = useMemo(() => createIOMachine, []);
   const [state, send] = useMachine(machine);
 
-  const { board, userLastTurn, step } = state.context;
+  const { board } = state.context;
   const rollDice = () => {
     const res = send("TURN");
     return res.context;
   };
+
+  useEffect(() => {
+    if (state.matches("pbWon")) {
+      alert("Plastic bottle won, please refresh the page to play again");
+    } else if (state.matches("userWon")) {
+      alert("you won! please refresh the page to play again");
+    }
+  }, [state]);
+
   return (
     <div style={{ maxWidth: "1350px", maxHeight: "850px" }}>
       <Header rollDice={rollDice} />
