0a92425bbb68f0bf8392fdab4bad22750b0d73d2
yoavweber <yoav.weber@code.berlin>
2023-03-06 18:43:29 +0200

    adding basic test for state machine

--- src/Game/stateMachine.test.ts
+++ src/Game/stateMachine.test.ts
@@ -2,27 +2,50 @@ import { Location, Entities } from "../Enteties/enteties";
 import { createGameMachine } from "./stateMachine";
 import { GenBoardLocationPerEntitiy } from "../Board/location";
 import { getBoardLocationData } from "../Board/board";
+import { DiceFuncs, Direction } from "../Dice/dice";
+import { interpret } from "xstate";
+import { compareObj } from "../logic/utils";
 
-const userLocation = { Column: 1, Row: 1 } as Location;
-const PBLocation = { Column: 5, Row: 5 } as Location;
-const GPgpLocation = { Column: 20, Row: 20 } as Location;
+const mockuserLocation = { Column: 1, Row: 1 } as Location;
+const mockPBLocation = { Column: 5, Row: 5 } as Location;
+const mockGPgpLocation = { Column: 20, Row: 20 } as Location;
 
-describe("testStateMachine", () => {
-  test("board get generated", () => {
+const mockMoveDice = () => 1;
+const mockDirectionDice = () => 2;
+
+describe("test state machine game", () => {
+  let gameStateMachine: ReturnType<typeof createGameMachine>;
+
+  beforeAll(() => {
     const boardLocationGen: GenBoardLocationPerEntitiy = {
-      [Entities.User]: () => userLocation,
-      [Entities.PB]: () => PBLocation,
-      [Entities.GPgp]: () => GPgpLocation,
+      [Entities.User]: () => mockuserLocation,
+      [Entities.PB]: () => mockPBLocation,
+      [Entities.GPgp]: () => mockGPgpLocation,
     };
-    const stateMachine = createGameMachine(boardLocationGen);
-    const { initialState } = stateMachine;
-    stateMachine.transition(initialState, "START");
-    const { board } = stateMachine.context;
-    expect(board).toBeTruthy();
-    if (board) {
-      expect(getBoardLocationData(board, userLocation)).toBe(Entities.User);
-      expect(getBoardLocationData(board, PBLocation)).toBe(Entities.PB);
-      expect(getBoardLocationData(board, GPgpLocation)).toBe(Entities.GPgp);
-    }
+    const genDice: DiceFuncs = {
+      move: mockMoveDice,
+      direction: mockDirectionDice,
+    };
+    const stateMachine = createGameMachine(boardLocationGen, genDice, genDice);
+
+    gameStateMachine = stateMachine;
+  });
+
+  test("pb makes move", (done) => {
+    const pbExpectedLocation: Location = { Row: 4, Column: 4 };
+    // console.log(gameStateMachine.context);
+    const interpretMachine = interpret(gameStateMachine).onTransition(
+      (context) => {
+        const { pbLocation } = context.context;
+        if (pbLocation && compareObj(pbLocation, pbExpectedLocation)) {
+          // assert that effects were executed
+          expect(true).toBeTruthy();
+          done();
+        }
+      }
+    );
+
+    interpretMachine.start();
+    interpretMachine.send(["START_GAME", "TURN"]);
   });
 });
