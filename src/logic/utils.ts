2559b33676672aab68c4466d8611120986d6e11a
yoavweber <yoav.weber@code.berlin>
2023-03-07 10:09:56 +0200

    exporting random generating function and creating compareObj function

--- src/logic/utils.ts
+++ src/logic/utils.ts
@@ -5,6 +5,18 @@ export const deepClone = <T>(items: MaybeArray<T>[]): MaybeArray<T>[] =>
     Array.isArray(item) ? deepClone(item) : item
   ) as MaybeArray<T>[];
 
-function randomIntFromInterval(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+export function randomIntFromInterval(min: number, max: number): number {
+  return Math.abs(Math.floor(Math.random() * (max - min + 1) + min));
 }
+
+export function compareObj<T extends {}>(obj1: T, obj2: T) {
+  return (
+    Object.keys(obj1).length === Object.keys(obj2).length &&
+    (Object.keys(obj1) as (keyof typeof obj1)[]).every((key) => {
+      return (
+        Object.prototype.hasOwnProperty.call(obj2, key) &&
+        obj1[key] === obj2[key]
+      );
+    })
+  );
+} 
